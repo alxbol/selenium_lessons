879b0d5ac66e6b464af6d95ed23ece46faadeb11
Alexey Bolshakov <bolshakov.alexey@gmail.com>
2021-11-22 15:15:00 +0300

    Задание 8. Сделайте сценарий, проверяющий наличие стикеров у товаров

---
+++ src/test/java/Task8Test.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Task8Test {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void start() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void task8Test() {
+        driver.get("http://localhost/litecart/en/");
+
+        List<WebElement> productElementList = driver.findElements(By.cssSelector("li[class^='product']"));
+        for (WebElement productElement : productElementList) {
+            Assert.assertEquals(productElement.findElements(By.cssSelector("div[class^='sticker']")).size(), 1);
+        }
+    }
+
+    @AfterClass
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
