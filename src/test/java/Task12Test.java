9f3a71804a82603c9005420b4edb8707a40667bf
Alexey Bolshakov <bolshakov.alexey@gmail.com>
2021-11-29 12:05:35 +0300

    [FIX] Задание 12. Сделайте сценарий добавления товара

--- src/test/java/Task12Test.java
+++ src/test/java/Task12Test.java
@@ -10,6 +10,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.sql.Timestamp;
 import java.time.Duration;
 
 public class Task12Test {
@@ -30,10 +31,9 @@ public class Task12Test {
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
 
-        //General tab
-        driver.findElement(By.cssSelector("#content > div:nth-child(7) > a:nth-child(2)")).click();
+        driver.findElement(By.cssSelector("a[href$='edit_product']")).click();
         driver.findElement(By.cssSelector("input[name='status']")).click();
-        String productName = "newProduct";
+        String productName = "newProduct" + new Timestamp(System.currentTimeMillis()).getTime();
         driver.findElement(By.cssSelector("input[name='name[en]']")).sendKeys(productName);
         driver.findElement(By.cssSelector("input[name='code']")).sendKeys("PD");
         driver.findElement(By.cssSelector("input[value='1-3']")).click();
@@ -63,7 +63,8 @@ public class Task12Test {
         select.selectByValue("USD");
 
         driver.findElement(By.cssSelector("button[name='save']")).click();
-        Assert.assertEquals(driver.findElements(By.xpath(String.format("//a[contains(text(),'%s'", productName))).size(), 1);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format("//a[contains(text(),'%s')]", productName))));
+        Assert.assertEquals(driver.findElements(By.xpath(String.format("//a[contains(text(),'%s')]", productName))).size(), 1);
     }
 
     @AfterClass
